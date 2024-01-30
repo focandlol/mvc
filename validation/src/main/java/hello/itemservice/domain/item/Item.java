054c4677ed98b2cb14443bb3fcfbf36068d0e370
focandlol <focandlol@naver.com>
2024-01-30 17:23:55 +0900

    sec05 bean validation

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,18 +1,36 @@
 package hello.itemservice.domain.item;
 
+import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000") //objectError 생성
+//springBoot 3.x 부터는 javascript 지원 안돼서 사용 불가
 public class Item {
 
+    //@NotNull(groups = UpdateCheck.class)
     private Long id;
+
+   // @NotBlank(message = "공백x", groups= {SaveCheck.class, UpdateCheck.class})//groups 설정 수정,저장시 validate 적용
     private String itemName;
+
+  //  @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+  //  @Range(min = 1000, max = 1000000,groups = {SaveCheck.class,UpdateCheck.class})
     private Integer price;
+
+   // @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+  //  @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
     }
-
+    @Builder
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
