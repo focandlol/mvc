5bf20c2b9bc847d5e242f40f7cc0ea64b5d1f8ce
focandlol <focandlol@naver.com>
2024-02-02 17:02:12 +0900

    sec07 filter, interceptor

---
+++ login/src/test/java/hello/login/ItemServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package hello.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+}
